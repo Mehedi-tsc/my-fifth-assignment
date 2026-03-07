53038781a494c76d77f14a66471996a2d8b48009
Mehedi-tsc <mehedimmc1@gmail.com>
2026-03-07 11:34:44 +0600

    first commit

---
+++ login.js
@@ -0,0 +1,15 @@
+const inputUserName = document.getElementById('input-name');
+const inputPassword = document.getElementById('input-password');
+const loginBtn = document.getElementById('btn-login');
+loginBtn.addEventListener('click',function(){
+    if(inputUserName.value!=='admin'){
+        alert('Please give the correct Username');
+    }
+    else if(inputPassword.value!=='admin123'){
+         alert('Please give the correct Password');
+    }
+    else if(inputUserName.value==='admin' && inputPassword.value==='admin123' ){
+       alert('login successfull');
+        window.location.assign("/home.html");
+    }
+})
