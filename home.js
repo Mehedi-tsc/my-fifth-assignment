dcae1a9c266502abb09bee65a0ccff7b78649b24
Mehedi-tsc <mehedimmc1@gmail.com>
2026-03-08 06:36:27 +0600

    Show active button and changing category icon

--- home.js
+++ home.js
@@ -1,4 +1,8 @@
 const allCardContainer = document.getElementById('all-card-container');
+const countAll = document.getElementById('count-all');
+const allBtn = document.getElementById('allBtn');
+const openBtn = document.getElementById('openBtn');
+const closedBtn = document.getElementById('closedBtn');
 const loadingCard = async() =>{
     const res = await fetch('https://phi-lab-server.vercel.app/api/v1/lab/issues');
     const data = await res.json();
@@ -6,6 +10,7 @@ const loadingCard = async() =>{
     displayCard(result);
 }
 function displayCard(card){
+    allCardContainer.innerHTML = "";
     card.forEach(item=> {
         const div = document.createElement('div');
         div.innerHTML = `
@@ -32,12 +37,50 @@ function displayCard(card){
                     <!-- badge part end -->
                      <hr class="my-1 text-[#E4E4E7]">
                      <div class="space-y-2 text-[#64748B]">
-                        <p>${item.author.toUpperCase()}</p>
+                        <p>${item.author.toUpperCase().split('_').join(' ')}</p>
                         <p>${item.createdAt}</p>
                      </div>
                 </div>
         `
         allCardContainer.appendChild(div);
+        
     })
+    countAll.innerText=card.length;
+    
 }
+async function openTab(){
+    const res = await fetch('https://phi-lab-server.vercel.app/api/v1/lab/issues');
+    const data = await res.json();
+    const result = data.data;
+    const newArray = result.filter(item => item.status==='open')
+    displayCard(newArray);
+}
+async function closeTab(){
+    const res = await fetch('https://phi-lab-server.vercel.app/api/v1/lab/issues');
+    const data = await res.json();
+    const result = data.data;
+    const newArray = result.filter(item => item.status==='closed')
+    displayCard(newArray);
+}
+allBtn.addEventListener('click',function(){
+    allBtn.classList.remove('text-[#64748B]');
+    allBtn.classList.add('btn-primary');
+    openBtn.classList.remove('btn-primary');
+    closedBtn.classList.remove('btn-primary');
+    loadingCard();
+})
+openBtn.addEventListener('click',function(){
+    openBtn.classList.remove('text-[#64748B]');
+    openBtn.classList.add('btn-primary');
+    closedBtn.classList.remove('btn-primary');
+    allBtn.classList.remove('btn-primary');
+    openTab();
+})
+closedBtn.addEventListener('click',function(){
+    closedBtn.classList.remove('text-[#64748B]');
+    closedBtn.classList.add('btn-primary');
+    openBtn.classList.remove('btn-primary');
+    allBtn.classList.remove('btn-primary');
+    closeTab();
+})
 loadingCard();
